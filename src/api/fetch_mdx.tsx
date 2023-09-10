fcd464e3325e1d33757ea84d70f38f03246322ce
zerodice0 <zerodice0@gmail.com>
2023-09-11 01:30:40 +0900

    - add fetch_mdx api.
    - mdx 파일을 가져오기 위한 fetch_mdx 추가.

---
+++ src/api/fetch_mdx.tsx
@@ -0,0 +1,23 @@
+import { cache } from "react";
+import fs from "fs/promises";
+import path from "path";
+import matter from "gray-matter";
+
+export const fetchMdx = cache(
+  async (fetchPath: string) => {
+    const fetched = await fs.readdir(fetchPath);
+
+    return await Promise.all(
+      fetched.filter((file: string) => path.extname(file) === ".mdx")
+        .map(async (file: string) => {
+          const filePath = `${fetchPath}/${file}`;
+
+          const fileContent = await fs.readFile(filePath, "utf8");
+          const { data, content } = matter(fileContent); 
+
+          return { ...data, body: content };
+        })
+    );
+  }
+
+);
