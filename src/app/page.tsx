97440332e96e0c1d56ed24d319989ff6ef82cf67
zerodice0 <zerodice0@gmail.com>
2023-09-12 00:58:02 +0900

    - test fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { fetchMdx } from "../api/fetch_mdx";
+
 import "./page.css";
 
-const Page = () => {
+const Page = async () => {
+  console.log(await fetchMdx("./src/public/notices"));
+
   return (
     <div className="content">
       <div className="aside">
