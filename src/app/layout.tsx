c9cd715034e4ff64401c31a0bcbbbc8389d46c85
zerodice0 <zerodice0@gmail.com>
2023-09-25 00:36:53 +0900

    remove authProvider from RootLayout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,37 @@
 import React from "react";
-import type { PropsWithChildren } from "react";
 import Navigation from "../components/navigation/navigation";
+
+import { getFirestore } from "firebase/firestore/lite";
+// import getFirebaseAdmin from "../firebase/firebaseAdmin";
+import * as admin from "firebase-admin";
+
 import "./layout.css";
-import { AuthProvider } from "../context/authProvider";
+
+import type { PropsWithChildren } from "react";
 
 const RootLayout = ({ children }: PropsWithChildren): JSX.Element => {
+  const firebaseAdminConfig = {
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+  };
+
+  console.log("admin.apps.length:  ", admin.apps.length);
+  if (admin.apps.length === 0) {
+    admin.initializeApp({
+      credential: admin.credential.cert(firebaseAdminConfig),
+      databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    });
+  }
+
+  const fireStore = getFirestore(admin.app, admin.firestore);
+
   return (
     <html>
-      <head>{"BlackBear's Archive"}</head>
+      <head></head>
       <body>
-        <AuthProvider>
-          <Navigation />
-          {children}
-        </AuthProvider>
+        <Navigation />
+        {children}
       </body>
     </html>
   );
