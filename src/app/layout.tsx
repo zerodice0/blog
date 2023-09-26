091358a109ca8a4b09e8fe768ed6f25bf723d67b
zerodice0 <zerodice0@gmail.com>
2023-09-27 08:59:52 +0900

    remove firebase admin config from layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,11 @@
 import React from "react";
 import Navigation from "../components/navigation/navigation";
 
-import { getFirestore } from "firebase/firestore/lite";
-// import getFirebaseAdmin from "../firebase/firebaseAdmin";
-import * as admin from "firebase-admin";
-
 import "./layout.css";
 
 import type { PropsWithChildren } from "react";
 
 const RootLayout = ({ children }: PropsWithChildren): JSX.Element => {
-  const firebaseAdminConfig = {
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-  };
-
-  console.log("admin.apps.length:  ", admin.apps.length);
-  if (admin.apps.length === 0) {
-    admin.initializeApp({
-      credential: admin.credential.cert(firebaseAdminConfig),
-      databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
-    });
-  }
-
-  const fireStore = getFirestore(admin.app, admin.firestore);
-
   return (
     <html>
       <head></head>
